990870697c5840edfbd5b590c03357265f7f8da8
ander0code <maxxd814@gmail.com>
2025-05-29 15:51:38 -0500

    version(init)0.000

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import StudentsViewSet
 
 router = DefaultRouter()
-router.register(r'', StudentsViewSet, basename='students')
+router.register(r'students', StudentsViewSet, basename='students')
 
 urlpatterns = [
     path('', include(router.urls)),
